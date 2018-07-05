e1bada940b56dddc19a41c45985c66e0d44f95dd
laobingm <laobingm@qq.com>
2018-07-05 17:29:28 +0800

    configure mysql conn info.

--- Blog/settings.py
+++ Blog/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'HOST': 'gz-cdb-mfro5399.sql.tencentcdb.com',
+        'PORT': '63292',
+        'USER': 'root',
+        'PASSWORD': 'a123456+',
     }
 }
 

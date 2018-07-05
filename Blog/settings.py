6a8085251f9187d8e575a59c93d832aed67a9031
laobingm <laobingm@qq.com>
2018-07-05 18:55:56 +0800

    add apps to INSTALLED_APPS configure.

--- Blog/settings.py
+++ Blog/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin',
+    'article',
+    'permission',
+    'role',
+    'user'
 ]
 
 MIDDLEWARE = [

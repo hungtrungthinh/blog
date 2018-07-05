b9fb810155e7abbd0ee026b5e5933617bcc53b57
laobingm <laobingm@qq.com>
2018-07-05 22:40:00 +0800

    add home app in order to access home page.

--- Blog/settings.py
+++ Blog/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'article',
     'role',
-    'user'
+    'user',
+    'home',
 ]
 
 MIDDLEWARE = [
--- Blog/urls.py
+++ Blog/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
-
+from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('article/', include('article.urls')),
+    path('user/', include('user.urls')),
+    path('role/', include('role.urls')),
 ]
